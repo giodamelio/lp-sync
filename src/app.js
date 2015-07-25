2b29c9e52f27828864893ed95a7b34e3333991ff
Gio d'Amelio <giodamelio@gmail.com>
2015-07-25 12:02:21 -0700

    React-router hello world

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+import { Link } from 'react-router';
+
 class App extends React.Component {
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <div>
+        <ul>
+          <li><Link to='/home'>Home!</Link></li>
+          <li><Link to='/test'>Test!</Link></li>
+        </ul>
+        <br />
+        {this.props.children}
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Router, Route } from 'react-router';
+import { history } from 'react-router/lib/HashHistory';
 
 import App from './app';
+import Home from './home';
+import Test from './test';
 
 document.addEventListener('DOMContentLoaded', function() {
   React.render(
-    <App></App>,
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <Route path="home" component={Home}/>
+        <Route path="test" component={Test}/>
+      </Route>
+    </Router>,
     document.body
   );
 });
