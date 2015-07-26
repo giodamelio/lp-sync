4c6d2e57162f2be12a989352fcba0f82c18e0dc5
Gio d'Amelio <giodamelio@gmail.com>
2015-07-26 11:12:19 -0700

    Add react-bootstrap

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
 import { Link } from 'react-router';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <ul>
-          <li><Link to='/home'>Home!</Link></li>
-          <li><Link to='/test'>Test!</Link></li>
-        </ul>
-        <br />
+        <Navbar brand="Let's Play Sync" inverse>
+          <Nav left eventKey={0}>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/test'>Test</Link></li>
+          </Nav>
+        </Navbar>
         {this.props.children}
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -6,14 +6,22 @@ import App from './app';
 import Home from './home';
 import Test from './test';
 
+let onUpdate = function() {
+  console.log(this.state.location.pathname);
+};
+
+let router = (
+  <Router history={history} onUpdate={onUpdate}>
+    <Route component={App}>
+      <Route path="/" component={Home} />
+      <Route path="/test" component={Test} />
+    </Route>
+  </Router>
+);
+
 document.addEventListener('DOMContentLoaded', function() {
   React.render(
-    <Router history={history}>
-      <Route path="/" component={App}>
-        <Route path="home" component={Home}/>
-        <Route path="test" component={Test}/>
-      </Route>
-    </Router>,
+    router,
     document.body
   );
 });
